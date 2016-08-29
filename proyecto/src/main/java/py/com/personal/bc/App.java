c6697dd8f1cb4a5c5810e7fafa0bfb5b62e537bf
Sofia Alvarenga <sofi.crischu@gmail.com>
2016-08-29 11:16:00 -0400

    Arregle devuelta el conflicto de App.java

--- proyecto/src/main/java/py/com/personal/bc/App.java
+++ proyecto/src/main/java/py/com/personal/bc/App.java
@@ -7,8 +7,7 @@ package py.com.personal.bc;
 public class App 
 {
     public static void main( String[] args )
-    {
+    { 
         System.out.println( "Hello World! Sofi, en capacitacion de GIT" );
-
     }
 }
