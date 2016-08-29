f0ff84d2f2b52b351388eb25ffedb1f98661965a
Sofia Alvarenga <sofi.crischu@gmail.com>
2016-08-29 11:49:13 -0400

    Agregar clase House

---
+++ proyecto/src/main/java/py/com/personal/bc/House.java
@@ -0,0 +1,15 @@
+package py.com.personal.bc;
+
+/**
+ * Hello world!
+ *
+ */
+public class House
+{
+
+    
+    public static void main( String[] args )
+    {
+        System.out.println( "House" );
+    }
+}
