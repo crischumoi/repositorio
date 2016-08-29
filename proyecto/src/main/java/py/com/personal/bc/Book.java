62f45cf9f2ff9dc8a412f8a7e5bb2adc9412e2f4
Sofia Alvarenga <sofi.crischu@gmail.com>
2016-08-29 10:58:16 -0400

    Clase Book creada

---
+++ proyecto/src/main/java/py/com/personal/bc/Book.java
@@ -0,0 +1,17 @@
+package py.com.personal.bc;
+
+/**
+ * Hello world!
+ *
+ */
+public class Book
+{
+    private String nombre;
+    private String autor;
+    private int anho;
+    
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Book" );
+    }
+}
