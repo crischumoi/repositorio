6eaab37455a4fda4d7f84c14492786b561a52f73
Sofia Alvarenga <sofi.crischu@gmail.com>
2016-08-29 11:30:35 -0400

    Cambio de otro-desarrollador mas

--- proyecto/src/main/java/py/com/personal/bc/App.java
+++ proyecto/src/main/java/py/com/personal/bc/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     { 
         System.out.println( "Hello World! Sofi, en capacitacion de GIT" );
+        System.out.println( "Hello de otro-desarrollador-mas" );
     }
 }
