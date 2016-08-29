95b765bed74f0aa2ee4a9cabc1287921c23118cf
Sofia Alvarenga <sofi.crischu@gmail.com>
2016-08-29 11:41:16 -0400

    Agregar clase Shape

---
+++ proyecto/src/main/java/py/com/personal/bc/Shape.java
@@ -0,0 +1,15 @@
+package py.com.personal.bc;
+
+/**
+ * Hello world!
+ *
+ */
+public class Shape
+{
+
+    
+    public static void main( String[] args )
+    {
+        System.out.println( "Shape" );
+    }
+}
